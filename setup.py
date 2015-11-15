e383e40327a9adae175e8c6280c5c0ab2ce9c99b
Alejandro Saucedo <axsauze@gmail.com>
2015-11-15 19:15:09 +0000

    added to pypi try 2

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from distutils.core import setup
 setup(
   name = 'silver',
   packages = ['silver'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'SilverCore API python wrapper',
   author = 'Alejandro Saucedo',
   author_email = 'alejandro@hackpartners.com.com',
   url = 'https://github.com/HackTrain/silver',
-  download_url = 'https://github.com/HackTrain/silver/tarball/0.1', 
+  download_url = 'https://github.com/HackTrain/silver/tarball/0.2', 
   keywords = ['silvercore', 'silver', 'hackpartners', 'hacktrain', 'hack', 'partners'], 
+  include_package_data=True,
+  install_requires=[
+        "pyxb",
+        "suds"
+    ],
   classifiers = [],
 )
