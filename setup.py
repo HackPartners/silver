69b9671b02f680bb2280a5eddf6b8f2a02f41d74
Alejandro Saucedo <axsauze@gmail.com>
2015-11-15 19:30:48 +0000

    added try 4

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'silver',
   packages = ['silver'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.3.1',
   description = 'SilverCore API python wrapper',
   author = 'Alejandro Saucedo',
   author_email = 'alejandro@hackpartners.com.com',
   url = 'https://github.com/HackTrain/silver',
-  download_url = 'https://github.com/HackTrain/silver/tarball/0.3', 
+  download_url = 'https://github.com/HackTrain/silver/tarball/0.3.1', 
   keywords = ['silvercore', 'silver', 'hackpartners', 'hacktrain', 'hack', 'partners'], 
-  package_data={'silver': ['silverraw/*', '*']},
+  package_data={'silver': ['silverraw/*.py', '*.py']},
   install_requires=[
         "pyxb",
         "suds"
