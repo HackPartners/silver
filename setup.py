1ad4a15c479a18994e7988802fcdda275d9a3ab4
Alejandro Saucedo <axsauze@gmail.com>
2015-11-15 18:54:23 +0000

    added licence and cfg

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-setup.py
+from distutils.core import setup
+setup(
+  name = 'silver',
+  packages = ['silver'], # this must be the same as the name above
+  version = '0.1',
+  description = 'SilverCore API python wrapper',
+  author = 'Alejandro Saucedo',
+  author_email = 'Alejandro@hackpartners.com.com',
+  url = 'https://github.com/HackTrain/silver',
+  download_url = 'https://github.com/HackTrain/silver/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
